52bcc4594f920fef46c1502e756e064e0a13faa1
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-10-30 19:39:38 +0000

    feat: first draft of contact form

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet, FieldTitle } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Contact() {
+    return (
+        <main>
+            <form className="max-w-lg mx-auto py-12">
+                <FieldSet>
+                    <FieldLegend className="sr-only">
+                        Contact Form
+                    </FieldLegend>
+                    <FieldTitle>
+                        <h2>Contact</h2>
+                    </FieldTitle>
+                    <FieldDescription>
+                        Contact me if you’re interested in an original artwork, commission or even just fancy saying hi!
+                    </FieldDescription>
+                    <FieldGroup>
+                            <Field>
+                                <FieldLabel htmlFor="name">
+                                    Full Name
+                                </FieldLabel>
+                                <Input id="name" type="text"/>
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="email">
+                                    Email
+                                </FieldLabel>
+                                <Input id="name" type="email"/>
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="subject">
+                                    Subject
+                                </FieldLabel>
+                                <Input id="name" type="text"/>
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="message">
+                                    Message
+                                </FieldLabel>
+                                <Textarea id="message" />
+                            </Field>
+                            <div>
+                                <Button 
+                                    className="w-auto min-w-[140px] cursor-pointer bg-brand hover:bg-brand/80 text-white font-bold tracking-wide"
+                                    type="submit">
+                                        SUBMIT
+                                </Button>
+                            </div>
+                    </FieldGroup>
+                </FieldSet>
+            </form>
+        </main>
+    );
+}
