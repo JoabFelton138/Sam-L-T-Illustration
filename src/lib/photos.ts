cf9ecd864c1bd285425aad319e1cf91a8f3ddd57
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-01-27 17:01:58 +0000

    chore: adding placeholder images to photography page

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -1,4 +1,154 @@
+import flowersOnBlue from "../../public/illustrations/flowers-on-blue.webp";
+import frogWaterskiing from "../../public/illustrations/frog-waterskiing.webp";
+import spooky from "../../public/illustrations/spooky.gif";
+
+import funkyMonkey from "../../public/illustrations/funky-monkey.webp";
+import ginghamGirl from "../../public/illustrations/gingham-girl.webp";
+import portraitLeft from "../../public/illustrations/portrait-left.webp";
+
+import portraitRight from "../../public/illustrations/portrait-right.webp";
+import bugs from "../../public/illustrations/bugs.webp";
+import tomYum from "../../public/illustrations/tom-yum.webp";
+
 import type { GalleryItem } from "./illustrations";
 
-// Placeholder until photos are added
-export const photos: GalleryItem[] = []
+export const photos: GalleryItem[] = [
+    {
+        image: tomYum,
+        title: "Tom Yum",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: flowersOnBlue,
+        title: "Flowers",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: funkyMonkey,
+        title: "Funky Monkey",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: portraitLeft,
+        title: "Portrait",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: spooky,
+        title: "Spooky",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: portraitRight,
+        title: "Portrait",
+        description: "Lorem ipsum dolor sit amet."
+    },
+
+    {
+        image: frogWaterskiing,
+        title: "Waterskiing",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: bugs,
+        title: "Bugs",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: ginghamGirl,
+        title: "Bugs",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: tomYum,
+        title: "Tom Yum",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: flowersOnBlue,
+        title: "Flowers",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: funkyMonkey,
+        title: "Funky Monkey",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: portraitLeft,
+        title: "Portrait",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: spooky,
+        title: "Spooky",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: portraitRight,
+        title: "Portrait",
+        description: "Lorem ipsum dolor sit amet."
+    },
+
+    {
+        image: frogWaterskiing,
+        title: "Waterskiing",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: bugs,
+        title: "Bugs",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: ginghamGirl,
+        title: "Bugs",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: tomYum,
+        title: "Tom Yum",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: flowersOnBlue,
+        title: "Flowers",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: funkyMonkey,
+        title: "Funky Monkey",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: portraitLeft,
+        title: "Portrait",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: spooky,
+        title: "Spooky",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: portraitRight,
+        title: "Portrait",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: frogWaterskiing,
+        title: "Waterskiing",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: bugs,
+        title: "Bugs",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        image: ginghamGirl,
+        title: "Bugs",
+        description: "Lorem ipsum dolor sit amet."
+    },
+
+]
