3714ea7b2f48b0459ecfee44f0cf28658992a9a1
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-01-30 20:27:42 +0000

    chore: removed unused import

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { useEffect, useRef } from "react";  
+import { useRef } from "react";  
 import { useFadeIn } from "@/hooks/use-fade-in";
 
 const formSchema = z.object({
