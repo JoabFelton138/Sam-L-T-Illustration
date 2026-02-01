afc248f95277da8c4489f27b9152040516701729
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-01 13:55:31 +0000

    feat: quantity select

--- src/components/ProductDialog.tsx
+++ src/components/ProductDialog.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import { Dialog, DialogContent, DialogDescription, DialogTitle } from "./ui/dialog";
 import { Button } from "./ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "./ui/select";
 
 interface ProductDialogProps {
     open: boolean;
@@ -11,6 +18,8 @@ interface ProductDialogProps {
     description: string;
     price: number;
 }
+
+const quantity = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const ProductDialog = ({open, onOpenChange, imageUrl, altText, title, description, price} : ProductDialogProps) => {
     return (
@@ -29,6 +38,22 @@ export const ProductDialog = ({open, onOpenChange, imageUrl, altText, title, des
                     <div className="bg-white p-6 overflow-y-auto flex flex-col gap-4 justify-center">
                         <DialogTitle>{title}</DialogTitle>
                         <p>£{price}</p>
+
+                        <div className="flex flex-row items-center gap-2">
+                            <label htmlFor="form-quantity" className="text-sm font-medium text-[#2C3E50] shrink-0">
+                                Quantity
+                            </label>
+                            <Select defaultValue="1">
+                                <SelectTrigger id="form-quantity" className="w-16">
+                                    <SelectValue />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {quantity.map((q) => 
+                                        <SelectItem key={q} value={q.toString()}>{q}</SelectItem>
+                                    )}
+                                </SelectContent>
+                            </Select>
+                        </div>
                         <p>{description}</p>
                         <Button 
                             className="w-fit cursor-pointer bg-brand hover:bg-brand/80 text-white font-bold tracking-wide">
