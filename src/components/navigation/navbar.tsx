620ad0f44df459a0f3d3472266646704fcb86c17
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-06 10:22:58 +0000

    style: refactored layout for nav so space inbetween social icons, nav links and logo are always present

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -5,6 +5,10 @@ import { SocialIcons } from "./social-icons";
 import { BasketCount } from "./basket-count";
 
 export function Navbar() {
+
+    const leftNavItems = menuItems.slice(0, 3);
+    const rightNavItems = menuItems.slice(3);
+
     return (
             <div className="flex flex-col justify-center items-center space-y-8 py-6 max-w-7xl mx-auto">    
                 <Link href="/">
@@ -23,23 +27,25 @@ export function Navbar() {
                         loading="eager"
                     />
                 </Link>
-                <div className="flex flex-row gap-8 sm:gap-10 md:gap-12 lg:gap-20 xl:gap-26">
-                    {menuItems.map((item) => 
-                        <div key={item.title} 
-                             className={item.title === "Basket" ? "flex items-center relative gap-1" : ""}>
-                            <Link 
-                                key={item.title} 
-                                href={item.url} 
+                <div className="flex flex-row justify-center items-center w-full">
+                    <div className="flex flex-row justify-end gap-6 md:gap-10 lg:gap-14 flex-1">
+                        {leftNavItems.map((navLink) => (
+                            <Link key={navLink.title} href={navLink.url} 
                                 className="tracking-widest font-medium text-[11px] sm:text-xs md:text-[13px] text-brand hover:text-brand-hover hover:bg-transparent">
-                                    {item.title.toUpperCase()}
+                                {navLink.title.toUpperCase()}
                             </Link>
-                            {item.title === "Basket" && (
-                                <span className="absolute -top-4 -right-4">
-                                    <BasketCount/>
-                                </span>
-                            )}
-                        </div>
+                        ))}
+                    </div>
+                    <div className="w-24 md:w-32 shrink0"/>
+                    <div className="flex flex-row justify-start gap-6 md:gap-10 lg:gap-14 flex-1">
+                        {rightNavItems.map((navLink) => (
+                            <Link key={navLink.title} href={navLink.url}    
+                                className="tracking-widest font-medium text-[11px] sm:text-xs md:text-[13px] text-brand hover:text-brand-hover hover:bg-transparent">
+                                {navLink.title.toUpperCase()}
+                            </Link>
+                        )
                     )}
+                    </div>
                 </div>
                 <SocialIcons gap="gap-5" />
             </div>
