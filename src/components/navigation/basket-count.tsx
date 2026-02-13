7461e79e4c157222d1ac8bab219782f25cb8e70a
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-13 17:28:27 +0000

    feat: updated basket count to use value derived from store

--- src/components/navigation/basket-count.tsx
+++ src/components/navigation/basket-count.tsx
@@ -1,3 +1,4 @@
+import { useBasketStore } from "@/store/basket-store";
 import { Badge } from "../ui/badge";
 
 interface BasketCountProps {
@@ -6,10 +7,13 @@ interface BasketCountProps {
     count?: number;
 }
 
-export const BasketCount = ({ count = 1, minWidth = "min-w-5", height = "h-5" }: BasketCountProps) => {
+export const BasketCount = ({ minWidth = "min-w-5", height = "h-5" }: BasketCountProps) => {
+    const count = useBasketStore((state) => 
+        state.items.reduce((acc, item) => acc + item.quantity, 0)
+    );
     return (
         <Badge className={`bg-brand text-white flex items-center justify-center text-xs px-1 ${minWidth} ${height}`}>
-            {count}
+            {count === 0 ? null : count}
         </Badge>
     )
 }
