54a2c2fe11b3dae4a2a93cc735587ad412438976
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-13 17:21:02 +0000

    feat: set up a central store to contain basket state

---
+++ src/store/basket-store.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { BasketItem, Product } from "@/types/types";
+
+interface BasketStore {
+    items: BasketItem[];
+    addItem: (product: Product, quantity: number) => void;
+}
+
+export const useBasketStore = create<BasketStore>()(
+    persist(
+        (set) => ({
+            items: [],
+            addItem: (product, quantity) => 
+                set((state) => {
+                    const existingItem = state.items.find(
+                        (item) => item.product.id === product.id
+                    );
+                    if (existingItem) {
+                        return {
+                            items: state.items.map((item) => 
+                                item.product.id === product.id
+                                ? {...item, quantity: item.quantity + quantity}
+                                : item
+                            ),
+                        };
+                    }
+                    return {
+                        items: [...state.items, {product, quantity}],
+                    }
+                }),
+        }),
+        {
+            name: "basket-storage",
+        }
+    )
+);
