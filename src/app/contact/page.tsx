9212ec29c29f80ab4b5c2d3175b0ca0562f81fd0
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-13 11:54:11 +0000

    style: style updates to form layout on devices with width < xl

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -48,9 +48,9 @@ export default function Contact() {
     };
 
     return (
-        <main className="flex flex-col xl:flex-row justify-center items-center gap-14 px-5 sm:px-6 lg:px-8 py-12 sm:py-5 max-w-7xl mx-auto">
-            <div ref={textRef} className="order-2 xl:order-1">                
-                <form className="flex-1 max-w-lg" onSubmit={form.handleSubmit(onSubmit)}>
+        <main className="flex flex-col lg:flex-row justify-center items-center gap-14 px-5 sm:px-6 lg:px-8 py-12 sm:py-5 max-w-7xl mx-auto">
+            <div ref={textRef}>                
+                <form className="flex-1 w-full max-w-xl lg:max-w-lg" onSubmit={form.handleSubmit(onSubmit)}>
                     <FieldSet>
                         <FieldLegend className="sr-only">
                             Contact Form
@@ -98,7 +98,7 @@ export default function Contact() {
                                         {form.formState.errors?.message?.message || "\u00A0"}
                                     </FieldError>
                                 </Field>
-                                <div className="pt-2">
+                                <div className="pt-2 flex justify-center lg:justify-start">
                                     <Button 
                                         className="w-auto min-w-[140px] cursor-pointer bg-brand hover:bg-brand/80 text-white font-bold tracking-wide"
                                         type="submit">
@@ -109,7 +109,7 @@ export default function Contact() {
                     </FieldSet>
                 </form>
             </div>
-            <div ref={imageRef} className="flex hidden md:block order-1 xl:order-2">                
+            <div ref={imageRef} className="flex hidden lg:block">                
                 <Image src="/eggy.webp" 
                    alt="Eggy" 
                    className="rounded object-contain w-full h-auto" 
