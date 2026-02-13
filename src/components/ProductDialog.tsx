5a98beab8ebbcda7c2740e7d4ccfd9ed9f521b62
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-13 17:23:17 +0000

    feat: state for selected quantity, replaced individual props with product, add item functionality for button and re-route to basket on other button click

--- src/components/ProductDialog.tsx
+++ src/components/ProductDialog.tsx
@@ -8,15 +8,15 @@ import {
     SelectTrigger,
     SelectValue,
 } from "./ui/select";
+import { useState } from "react";
+import {useRouter} from "next/navigation";
+import { useBasketStore } from "@/store/basket-store";
+import { Product } from "@/types/types";
 
 interface ProductDialogProps {
     open: boolean;
     onOpenChange: (open: boolean) => void;
-    imageUrl: string;
-    altText?: string;
-    title: string;
-    description: string;
-    price: number;
+    product: Product | null;
 }
 
 const quantity = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -25,26 +25,29 @@ const buttonClass = "w-fit cursor-pointer bg-brand hover:bg-brand/80 text-white 
 export const ProductDialog = ({
     open,
     onOpenChange,
-    imageUrl,
-    altText,
-    title,
-    description,
-    price,
+    product
 }: ProductDialogProps) => {
+
+    const [selectedQuantity, setSelectedQuantity] = useState(1);
+    const addItem = useBasketStore((state) => state.addItem);
+    const router = useRouter();
+
+    if (!product) return null;
+    
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
             <DialogContent className="w-[95vw] max-w-lg sm:max-w-xl md:max-w-3xl lg:max-w-5xl max-h-[90vh] p-0 overflow-hidden border-0 shadow-none">                
                 <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] min-h-0">
                     <Image
-                        src={imageUrl}
-                        alt={altText ?? ""}
+                        src={product.featuredImage?.url ?? ""}
+                        alt={product.featuredImage.altText ?? ""}
                         width={500}
                         height={500}
                         className="w-full h-full object-cover max-h-[50vh] md:max-h-none"
                     />
                     <div className="bg-white p-6 overflow-y-auto flex flex-col gap-4 justify-center">
-                        <DialogTitle>{title}</DialogTitle>
-                        <p>£{price}</p>
+                        <DialogTitle>{product.title}</DialogTitle>
+                        <p>£{product.priceRange.minVariantPrice.amount}</p>
                         <div className="flex flex-row items-center gap-2">
                             <label
                                 htmlFor="form-quantity"
@@ -52,7 +55,10 @@ export const ProductDialog = ({
                             >
                                 Quantity
                             </label>
-                            <Select defaultValue="1">
+                            <Select 
+                                value={selectedQuantity.toString()}
+                                onValueChange={(value) => setSelectedQuantity(parseInt(value))}
+                            >
                                 <SelectTrigger
                                     id="form-quantity"
                                     className="w-16"
@@ -71,12 +77,22 @@ export const ProductDialog = ({
                                 </SelectContent>
                             </Select>
                         </div>
-                        <p>{description}</p>
+                        <p>{product.description}</p>
                         <div className="flex flex-row items-center gap-2">
-                            <Button className={buttonClass}>
+                            <Button 
+                                className={buttonClass}
+                                onClick={() => {
+                                    addItem(product, selectedQuantity);
+                                    onOpenChange(false);
+
+                                }}
+                            >
                                 Add to Basket
                             </Button>
-                            <Button className={buttonClass}>
+                            <Button 
+                                className={buttonClass}
+                                onClick={() => router.push("/basket")}
+                            >
                                 View Basket
                             </Button>
                         </div>
