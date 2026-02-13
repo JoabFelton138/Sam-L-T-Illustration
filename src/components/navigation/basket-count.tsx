29781b1b4da21b12b6adced9ad67c9e5913e2705
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-13 19:50:07 +0000

    bug: empty badge would display if count === 0

--- src/components/navigation/basket-count.tsx
+++ src/components/navigation/basket-count.tsx
@@ -8,12 +8,16 @@ interface BasketCountProps {
 }
 
 export const BasketCount = ({ minWidth = "min-w-5", height = "h-5" }: BasketCountProps) => {
+    
     const count = useBasketStore((state) => 
         state.items.reduce((acc, item) => acc + item.quantity, 0)
     );
+    
     return (
-        <Badge className={`bg-brand text-white flex items-center justify-center text-xs px-1 ${minWidth} ${height}`}>
-            {count === 0 ? null : count}
-        </Badge>
-    )
+        count > 0 ? (
+            <Badge className={`bg-brand text-white flex items-center justify-center text-xs px-1 ${minWidth} ${height}`}>
+                {count}
+            </Badge>
+        ) : null
+    );
 }
