24653d686be333e0362bd75acbc006ba1de3bec2
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-02-15 20:00:12 +0000

    style: tidied layout for contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,6 +10,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useRef } from "react";  
 import { useFadeIn } from "@/hooks/use-fade-in";
+import { Header } from "@/components/Header";
 
 const formSchema = z.object({
     name: z.string().regex(/^[a-zA-Z]{2,}\s+[a-zA-Z]{2,}$/, "Name must be two words, each at least 2 characters"),    
@@ -48,26 +49,23 @@ export default function Contact() {
     };
 
     return (
-        <main className="flex flex-col lg:flex-row justify-center items-center gap-14 px-5 sm:px-6 lg:px-8 py-12 sm:py-5 max-w-7xl mx-auto">
-            <div ref={textRef}>                
-                <form className="flex-1 w-full max-w-xl lg:max-w-lg" onSubmit={form.handleSubmit(onSubmit)}>
-                    <FieldSet>
-                        <FieldLegend className="sr-only">
-                            Contact Form
-                        </FieldLegend>
-                        <FieldTitle>
-                            <h2>Contact</h2>
-                        </FieldTitle>
-                        <FieldDescription>
-                            {`Contact me if you're interested in an original artwork, commission or even just fancy saying hi!`}
-                        </FieldDescription>
-                        <FieldGroup className="gap-4">
+        <main className="max-w-5xl mx-auto px-4 py-12">
+            <Header title="Contact" />
+            
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-14 items-center justify-items-center lg:justify-items-start">
+                <div ref={textRef} className="w-full max-w-lg">                
+                    <form className="w-full" onSubmit={form.handleSubmit(onSubmit)}>
+                        <FieldSet>
+                            <FieldLegend className="sr-only">
+                                Contact Form
+                            </FieldLegend>
+                            <FieldGroup className="gap-2">
                                 <Field>
                                     <FieldLabel htmlFor="name">
                                         Full Name
                                     </FieldLabel>
                                     <Input id="name" type="text" {...form.register("name")}/>
-                                    <FieldError >
+                                    <FieldError>
                                         {form.formState.errors?.name?.message || "\u00A0"}
                                     </FieldError>
                                 </Field>
@@ -76,7 +74,7 @@ export default function Contact() {
                                         Email
                                     </FieldLabel>
                                     <Input id="email" type="email" {...form.register("email")}/>
-                                    <FieldError >
+                                    <FieldError>
                                         {form.formState.errors?.email?.message || "\u00A0"}
                                     </FieldError>
                                 </Field>
@@ -85,7 +83,7 @@ export default function Contact() {
                                         Subject
                                     </FieldLabel>
                                     <Input id="subject" type="text" {...form.register("subject")}/>
-                                    <FieldError >
+                                    <FieldError>
                                         {form.formState.errors?.subject?.message || "\u00A0"}
                                     </FieldError>
                                 </Field>
@@ -94,7 +92,7 @@ export default function Contact() {
                                         Message
                                     </FieldLabel>
                                     <Textarea id="message" {...form.register("message")}/>
-                                    <FieldError >
+                                    <FieldError>
                                         {form.formState.errors?.message?.message || "\u00A0"}
                                     </FieldError>
                                 </Field>
@@ -105,17 +103,20 @@ export default function Contact() {
                                             SUBMIT
                                     </Button>
                                 </div>
-                        </FieldGroup>
-                    </FieldSet>
-                </form>
-            </div>
-            <div ref={imageRef} className="flex hidden lg:block">                
-                <Image src="/eggy.webp" 
-                   alt="Eggy" 
-                   className="rounded object-contain w-full h-auto" 
-                   width={420} 
-                   height={110} 
-                />
+                            </FieldGroup>
+                        </FieldSet>
+                    </form>
+                </div>
+                
+                <div ref={imageRef} className="hidden lg:block">                
+                    <Image 
+                        src="/eggy.webp" 
+                        alt="Eggy" 
+                        className="rounded object-contain w-full h-auto" 
+                        width={450} 
+                        height={140} 
+                    />
+                </div>
             </div>
         </main>
     );
